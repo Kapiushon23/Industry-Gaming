4c2bf7abdf0c451b8743d8132e48e50573f8e9a7
Kapiushon23 <kapiushon83@gmail.com>
2024-06-09 02:34:49 -0400

    Delete Engine directory

--- Engine/Scripts/trailers.js
+++
@@ -1,9 +0,0 @@
-function openTrailerModal(src) {
-    document.getElementById('trailerPreview').src = src;
-    document.getElementById('trailerPreviewModal').style.display = 'flex';
-}
-
-function closeTrailerModal() {
-    document.getElementById('trailerPreview').src = '';
-    document.getElementById('trailerPreviewModal').style.display = 'none';
-}
